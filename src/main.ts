b9694090bc4d5eeddccc29035bf63d7452c8593e
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-10-14 19:05:55 +0330

    :wrench: config: read the port from env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import * as config from 'config';
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
+
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
+  const port = process.env.PORT || serverConfig.port;
+  await app.listen(port);
+  logger.log(`Aplication listening on port ${port}`);
 }
 bootstrap();
