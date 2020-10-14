b4483289806e901132e5d7fb79908b1c6dedbab8
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-10-14 18:31:59 +0330

    :wrench: config: change db url config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,6 +11,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   password: process.env.DB_PASSWORD || dbConfig.password,
   database: process.env.DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  // url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URL,
   synchronize: process.env.DB_SYNCHRONIZE || dbConfig.synchronize,
 };
